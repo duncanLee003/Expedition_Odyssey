6b55c65ef83f08503d494f7fd8ad06b240aba1d8
duncanLee003 <duncanlee003@gmail.com>
2026-05-20 12:12:48 +0100

    TRJBD

--- levels/end_screen.gd
+++ levels/end_screen.gd
@@ -19,3 +19,7 @@ func _on_next_button_pressed():
 func _on_menu_button_pressed():
 	get_tree().paused = false
 	get_tree().change_scene_to_file("res://ui/main_menu.tscn")
+
+
+func _on_quit_button_pressed() -> void:
+	get_tree().quit()
